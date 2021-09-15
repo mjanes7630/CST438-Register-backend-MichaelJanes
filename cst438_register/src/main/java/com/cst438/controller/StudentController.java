96a5e63fd781ef304ed0b2daa82158ada500e7b5
Michael Janes <mjanes7630@gmail.com>
2021-09-14 18:02:51 -0700

    Student Controller competed :)

---
+++ cst438_register/src/main/java/com/cst438/controller/StudentController.java
@@ -0,0 +1,66 @@
+package com.cst438.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.cst438.domain.Course;
+import com.cst438.domain.CourseRepository;
+import com.cst438.domain.Enrollment;
+import com.cst438.domain.EnrollmentDTO;
+import com.cst438.domain.EnrollmentRepository;
+import com.cst438.domain.ScheduleDTO;
+import com.cst438.domain.Student;
+import com.cst438.domain.StudentRepository;
+import com.cst438.service.GradebookService;
+
+@RestController
+public class StudentController
+{
+   @Autowired
+   CourseRepository courseRepository;
+   
+   @Autowired
+   StudentRepository studentRepository;
+   
+   @Autowired
+   EnrollmentRepository enrollmentRepository;
+   
+   @Autowired
+   GradebookService gradebookService;
+   
+   @PostMapping("/student/add")
+   @Transactional
+   public Student addStudent(@RequestParam String email, @RequestParam String name) {
+      
+      Student studentExist = studentRepository.findByEmail(email);
+      if(studentExist != null) {
+         throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Studnet already exists.");
+      }         
+      Student student = new Student();
+      student.setEmail(email);
+      student.setName(name);
+      studentRepository.save(student);
+         
+      return student;      
+   }
+ 
+   @PostMapping("/student/status")
+   @Transactional
+   public Student setStudentReg(@RequestParam String email, @RequestBody int stsCode) {
+      Student student = studentRepository.findByEmail(email);
+      if(student != null) {
+         student.setStatusCode(stsCode);
+         studentRepository.save(student);
+         return student;
+      } else {
+         throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Studnet doesn't exists.");
+      }
+   }
+}
