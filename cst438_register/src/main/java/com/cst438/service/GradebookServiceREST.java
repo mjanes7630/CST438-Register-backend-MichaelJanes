f66397bcad2acd96af98f4714d97bf67e48a92ec
Michael <mjanes@csumb.edu>
2021-09-26 18:40:11 -0700

    Assignment 4
    CourseController
    GradebookServiceREST
    gradebookSServiceMQ
    edited application prop

--- cst438_register/src/main/java/com/cst438/service/GradebookServiceREST.java
+++ cst438_register/src/main/java/com/cst438/service/GradebookServiceREST.java
@@ -3,12 +3,15 @@ package com.cst438.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
+import com.cst438.domain.Enrollment;
 import com.cst438.domain.EnrollmentDTO;
 
 
 public class GradebookServiceREST extends GradebookService {
 
 	private RestTemplate restTemplate = new RestTemplate();
+	
+	
 
 	@Value("${gradebook.url}")
 	String gradebook_url;
@@ -20,7 +23,10 @@ public class GradebookServiceREST extends GradebookService {
 	@Override
 	public void enrollStudent(String student_email, String student_name, int course_id) {
 		
-		//TODO  complete this method in homework 4
+	   EnrollmentDTO enrollmentDTO = new EnrollmentDTO(student_email, student_name, course_id);
+	   
+	   //TODO:make rest template send thingy work 
+		restTemplate.postForEntity(gradebook_url + "/enrollment", enrollmentDTO, null);
 		
 	}
 
